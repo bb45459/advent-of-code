1f2e9a71dd73646505586461b0e5516ed4d73ebd
Bill Bloebaum <bill.bloebaum@dialexa.com>
2021-12-06 19:38:59 -0600

    clean up

--- 2021/Day6/d6.js
+++ 2021/Day6/d6.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const data = fs.readFileSync("./input.txt", "utf-8");
 
-
 // const data = `3,4,3,1,2`;
 
 class Fish {
@@ -44,21 +43,12 @@ const school = new School(parseData)
 function printAges(school) {
     return school.fishes.map(el => el.timer);
 }
-// school.checkFish();
-// console.log(printAges(school))
-// school.ageFish();
-// console.log(printAges(school))
-// school.checkFish();
-// console.log(printAges(school))
 
 const days = 80;
 
 for (let i = 1; i < days; i++) {
-    // console.log(`days: `, i)
     school.ageFish();
-    // console.log(printAges(school))
     school.checkFish();
-    // console.log(printAges(school))
 }
 
 console.log(school.fishes.length)
@@ -67,7 +57,6 @@ const arr = Array(9).fill(0);
 parseData.forEach(el => arr[el] += 1)
 let reducer = arr;
 for (let i = 1; i < 257; i++) {
-    console.log('After ', i, 'Days')
     const copy = reducer.map(el => el);
     reducer.forEach((fish, i) => {
         if (i === 8) {
@@ -77,14 +66,9 @@ for (let i = 1; i < 257; i++) {
         reducer[i] = copy[i + 1];
         if (i === 6) {
             reducer[i] += copy[0]
-            // console.log(copy)
-            // console.log(reducer)
-            // console.log(copy[i])
-            // reducer[i - 1] += reducer[i];
             return;
         }
     })
-    // console.log(reducer)
     console.log(reducer.reduce((acc, curr) => acc + curr))
 }
 console.log(reducer.reduce((acc, curr) => acc + curr))
