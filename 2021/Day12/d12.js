4d2cfa61c69e07ce35cf41b40a0d063e9d2eab9d
Bill Bloebaum <bill.bloebaum@dialexa.com>
2021-12-13 18:08:10 -0600

    clean up

--- 2021/Day12/d12.js
+++ 2021/Day12/d12.js
@@ -45,58 +45,42 @@ start-RW`
 // const data = mock3;
 const data = input;
 const split = data.split('\n').filter(x => x).map(el => el.split('-'));
-// console.log(split)
 const nodeMapping = {};
 const start = split.filter(el => (el[0] === 'start' || el[1] === 'start'))
-// console.log(start)
 const startingRunners = start.map(el => el[0] === 'start' ? [el[1]] : [el[0]])
-// console.log(startingRunners)
 
 split.filter(el => (el[0] !== 'start' && el[1] !== 'start')).forEach(el => {
     nodeMapping[el[0]] = [...(nodeMapping[el[0]] || []), el[1]]
     nodeMapping[el[1]] = [...(nodeMapping[el[1]] || []), el[0]]
 })
 nodeMapping['end'] = [];
-// console.log(nodeMapping)
 
 
-function isLower(str) {
-    const str2 = str;
-    return str.toLowerCase() === str2 && str !== 'end'
+function isLowerAndNotEnd(str) {
+    return str.toLowerCase() === str && str !== 'end'
 }
 
 /*
-A, b
-Ac, Aend, Ab
-AcA, Abd
+Take an array of active and complete runs
+Check current node of each run
+Create new run for each valid next node
 */
-const nodes = {
-    'start': ['A', 'b'],
-    'A': ['b', 'c', 'end'],
-    'b': ['A', 'd', 'end'],
-    'c': ['A'],
-    'd': ['b'],
-    'end': []
-};
-
 function addNewNodes(runner, doubleAllowed) {
-    // console.log(runner)
-    const maxTimes = (doubleAllowed && !anyDoubleLowers(runner)) ? 2 : 1;
-    const last = runner[runner.length - 1];
-    // console.log(last)
-    if (last === 'end') return [runner];
-    // const next = nodes[last];
-    const next = nodeMapping[last];
-    // console.log(next)
-    const newRunner = next.filter(el => !(isLower(el) && runner.filter(stop => stop === el).length >= maxTimes)).map(el => {
-        return [...runner, el]
-    })
-    // console.log(newRunner)
-    return newRunner
+    const maxVisits = (doubleAllowed && !anyDoubleLowers(runner)) ? 2 : 1;
+    const currentNode = runner[runner.length - 1];
+    if (currentNode === 'end') return [runner];
+    const nextNodes = nodeMapping[currentNode];
+    const newRunners = nextNodes
+        .filter(el => !(isLowerAndNotEnd(el) && runner.filter(stop => stop === el).length >= maxVisits))
+        .map(el => {
+            return [...runner, el]
+        })
+    return newRunners
 }
 
+// Check if array contains any lower case copies
 function anyDoubleLowers(arr) {
-    const lowers = arr.filter(el => isLower(el));
+    const lowers = arr.filter(el => isLowerAndNotEnd(el));
     if (lowers.length === 0) return false;
     const hasDouble = lowers.reduce((acc, curr) => {
         if (acc.ans) return acc;
@@ -111,24 +95,21 @@ function anyDoubleLowers(arr) {
     return hasDouble.ans
 }
 
-let starter = startingRunners;
+let runners = startingRunners;
 let done = false;
 do {
-    let copy = starter.map(el => el);
-    starter = starter.map(runner => addNewNodes(runner, false)).flat()
-    // console.log(starter)
-    done = copy.every((el, idx) => el.length === starter[idx].length);
+    const copy = runners.map(el => el);
+    runners = runners.map(runner => addNewNodes(runner, false)).flat()
+    done = copy.every((el, idx) => el.length === runners[idx].length);
 } while (!done)
-console.log('part1:', starter.length)
+console.log('part1:', runners.length)
 
-starter = startingRunners;
+runners = startingRunners;
 done = false;
 do {
-    let copy = starter.map(el => el);
-    starter = starter.map(runner => addNewNodes(runner, true)).flat()
-    // console.log(starter)
-    done = copy.every((el, idx) => el.length === starter[idx].length);
+    const copy = runners.map(el => el);
+    runners = runners.map(runner => addNewNodes(runner, true)).flat()
+    done = copy.every((el, idx) => el.length === runners[idx].length);
 } while (!done)
 
-// console.log(starter)
-console.log('part2:', starter.length)
+console.log('part2:', runners.length)
